12fab7b387384327c7c8d1ecd7375722f393f540
watermelon <1095709389@qq.com>
2017-02-23 16:20:38 +0800

    修改图标和名称。

--- app/src/main/java/com/example/yifanweather/WeatherActivity.java
+++ app/src/main/java/com/example/yifanweather/WeatherActivity.java
@@ -242,7 +242,7 @@ public class WeatherActivity extends AppCompatActivity {
         weatherLayout.setVisibility(View.VISIBLE);
         Intent intent = new Intent(this, AutoUpdateService.class);
         startService(intent);
-    
+
     }
 
 }
