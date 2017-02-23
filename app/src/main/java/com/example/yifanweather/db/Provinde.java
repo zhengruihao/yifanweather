3915231d4bcedc09c1569ea8d297f0987762fab2
watermelon <1095709389@qq.com>
2017-02-23 14:12:00 +0800

    加入创建数据库和表的各各项配置。

---
+++ app/src/main/java/com/example/yifanweather/db/Provinde.java
@@ -0,0 +1,39 @@
+package com.example.yifanweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by watermelon on 2017/2/23.
+ */
+
+public class Provinde extends DataSupport {
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
